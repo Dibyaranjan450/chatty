8867c88cd1451e670679ae635e9cd91c3679ec26
dibyaranjan450 <dibyaranjan450@gmail.com>
2025-05-25 16:36:41 +0530

    Test: frontend CORS cokkie set

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.SOCKET_CORS_ORIGIN, credentials: true }));
 
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
