c42f44d608af451c221103cb304bcf1133a546a7
dibyaranjan450 <dibyaranjan450@gmail.com>
2025-05-25 18:19:41 +0530

    Test: SOCKET_CORS_ORIGIN

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
+console.log("SOCKET_CORS_ORIGIN", process.env.SOCKET_CORS_ORIGIN);
 app.use(cors({ origin: process.env.SOCKET_CORS_ORIGIN, credentials: true }));
 
 app.use("/api/auth", authRouter);
