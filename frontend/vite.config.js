1b94e5731246d3a104e3887465f8fe258bb1c9aa
dibyaranjan450 <dibyaranjan450@gmail.com>
2025-05-25 20:22:26 +0530

    Fixed: safari cookie issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/",
-  plugins: [react(), tailwindcss()],
-});
+export default ({ mode }) => {
+  // Load environment variables based on the current mode (development/production)
+  const env = loadEnv(mode, process.cwd());
+
+  console.log("VITE_SOCKET_BASE_URL", env.VITE_SOCKET_BASE_URL);
+
+  return defineConfig({
+    base: "/",
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_SOCKET_BASE_URL,
+          changeOrigin: true,
+          secure: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  });
+};
